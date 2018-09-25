d542ee124268c8eb516ddadfc28c6fa7ace58e72
Greg Orvik <gvorvik@gmail.com>
2018-09-25 18:48:23 -0500

    bring in song list component to index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,2 +1,4 @@
+const mongoose = require('mongoose');
+mongoose.plugin(schema => schema.options.usePushEach = true);
 require('./song');
 require('./lyric');
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://greg16:greg16@ds047632.mlab.com:47632/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
