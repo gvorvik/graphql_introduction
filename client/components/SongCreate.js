5553768120e35806aead8740df69c8d253fd558a
Greg Orvik <gvorvik@gmail.com>
2018-09-26 10:44:29 -0500

    update home page when new song is added

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+import {Link, hashHistory} from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
     constructor(props) {
@@ -10,22 +14,43 @@ class SongCreate extends Component {
 
     }
 
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.mutate({
+            variables: {title: this.state.songTitle},
+            refetchQueries: [{ query }]
+        })
+        .then(() => hashHistory.push('/'))
+        .catch(err => console.log(err));
+    }
+
 
     render() {
         return (
             <div>
+                <Link to="/">Back</Link>
                 <h1>Create a Song</h1>
-                <form>
+                <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
                     <input 
                         value={this.state.title} 
                         onChange={event => this.setState({songTitle: event.target.value})} 
                         type="text"    
                     />
+                    <input type="submit"/>
                 </form>
             </div>
         );
     }
 }
 
-export default SongCreate;
+const mutation = gql`
+    mutation AddSong($title: String){
+        addSong(title: $title){
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
